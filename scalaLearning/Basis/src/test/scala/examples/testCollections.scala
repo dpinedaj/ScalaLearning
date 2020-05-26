5ffcbf88de576b71661ade1f2ea40926ad680766
Daniel Pineda <dpinedaj@unal.edu.co>
2020-05-25 20:55:27 -0500

    _

--- scalaLearning/Basis/src/test/scala/examples/testCollections.scala
+++ scalaLearning/Basis/src/test/scala/examples/testCollections.scala
@@ -1,2 +1,13 @@
 package examples
 
+import examples.{Tuples, Lists, Maps, Sets, Arrays}
+
+object TestCollections {
+  def main(args: Array[String]): Unit = {
+    val tuples = Tuples
+    val lists = Lists
+    val maps = Maps
+    val sets = Sets
+    val arrays = Arrays
+  }
+}
