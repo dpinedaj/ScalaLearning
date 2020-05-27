ae4b04020d2f9ce95130456e9bdde06b2e803123
Daniel Pineda <dpinedaj@unal.edu.co>
2020-05-27 11:22:48 -0500

    _

---
+++ scalaLearning/Basis/src/main/scala/examples/Options.scala
@@ -0,0 +1,15 @@
+package examples
+
+object Obtions {
+    //An option represent the existence of a value
+    val opt = Option("Jamie")//res0: Option[String] = Some(Jamie)
+    println(opt.get) //res0: String = Jamie
+
+    //To ensure the existence of the value without error
+    println(opt.getOrElse("Jamie"))// res0: String = Jamie 
+
+    //The option Null cant be accessed by get
+    val none = Option(null) //res0:Option[Null] = None
+    
+
+}
--- scalaLearning/Basis/src/main/scala/examples/Tuples.scala
+++ scalaLearning/Basis/src/main/scala/examples/Tuples.scala
@@ -6,4 +6,17 @@ object Tuples {
     val junkTup = (6, 3.14, "elephant", true)
     //accessing index
     println(junkTup._2)//res0: Double = 3.14
+    
+    //Tuple2 definition
+    val t2 = Tuple2(1, "a")//res0: (Int, String) = (1, "a")
+
+    //To deconstruct a tuple
+    val (e1, e2) = t2//res0: e1: Int = 1 e2: String = a
+
+    //Tuple2 commonly called a pair
+    //Also can be defined tuples as arrow indicator
+    val t = (2->"b") //res0: (Int, String) = (2, "b")
+
+    val t3 = (3 -> "c" -> 4) //res0: ((Int, String), Int) = ((3,c),4)
+
 }
