7fce7a10d42ddfdab22d222e0b3f2ba85dc87314
Daniel Pineda <dpinedaj@unal.edu.co>
2020-08-23 15:19:59 -0500

    _

---
+++ tests/simpleChallenges/.ammonite/scala-2.12.12/amm-2.2.0/src/main/scala/tests/src/ammonite/$file/src/main/scala/tests.scala
@@ -0,0 +1,154 @@
+
+package ammonite
+package $file.src.main.scala
+import _root_.ammonite.interp.api.InterpBridge.{
+  value => interp
+}
+import _root_.ammonite.interp.api.InterpBridge.value.{
+  exit
+}
+import _root_.ammonite.interp.api.IvyConstructor.{
+  ArtifactIdExt,
+  GroupIdExt
+}
+import _root_.ammonite.runtime.tools.{
+  browse,
+  grep,
+  time,
+  tail
+}
+import _root_.ammonite.repl.tools.{
+  desugar,
+  source
+}
+import _root_.ammonite.main.Router.{
+  doc,
+  main
+}
+import _root_.ammonite.repl.tools.Util.{
+  pathScoptRead
+}
+
+
+object tests{
+/*<script>*/val points = Seq((0, 0), (0, 1), (1, 1), (1, 0))
+
+/*<amm>*/val res_1 = /*</amm>*/points.map(x => x._1 + x._2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*</script>*/ /*<generated>*/
+def $main() = { scala.Iterator[String]() }
+  override def toString = "tests"
+  /*</generated>*/
+}
--- tests/simpleChallenges/src/main/scala/tests.sc
+++ tests/simpleChallenges/src/main/scala/tests.sc
@@ -1,13 +1,19 @@
-val points = Seq((0, 0), (0, 1), (1, 1), (1, 0))
+def pascal(k: Int): Unit = {
+    for (row <- 1 to k){
+        for (column <- 1 to k){
+            print(iterPascal(row, column, k))
+        }
+        println()
+    }
+}
 
-points.map(x => x._1 + x._2)
+def iterPascal(row: Int, column: Int, k: Int): Int = {
+    if (column == 1 || column == k) 1
+    else iterPascal(row - 1, column - 1, k) + iterPascal(row, column - 1, k)
+}
 
 
-
-
-
-
-
+pascal(4)
 
 
 
