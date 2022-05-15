d131d34a9021a7743a6c93a2d131a43839baed73
Daniel Pineda Jaramillo <52385896+dpinedaj@users.noreply.github.com>
2022-05-15 10:59:49 -0500

    Update PascalTriangle.scala

--- tests/simpleChallenges/src/main/scala/hackerRank/functional/Easy/PascalTriangle.scala
+++ tests/simpleChallenges/src/main/scala/hackerRank/functional/Easy/PascalTriangle.scala
@@ -1,8 +1,11 @@
 package hackerRank.functional.Easy
 
 import scala.annotation.tailrec
+import scala.collection.mutable.{Map => MutableMap}
 
-object PascalTriangle extends App {
+case class Block(row: Int, column: Int)
+
+object PascalTriangle{
   /*
     This solution allows to perform the Pascal Triangle.
     i.e for a defined K = 5
@@ -13,27 +16,41 @@ object PascalTriangle extends App {
         1 3 3 1
         1 4 6 4 1
    */
+  val memoizeMap: MutableMap[Block, Int] = MutableMap.empty[Block, Int]
+  val initialRow: Int = 1
+  val initialColumn: Int = 1
 
-  pascal(5)
+  def main(args: Array[String]): Unit = {
+    pascal(30)
+  }
+
+  def pascal(lastRow: Int): Unit = {
+    pascal(lastRow, initialRow, initialColumn)
+  }
 
   @tailrec
-  def pascal(k: Int, row: Int = 1, column: Int = 1): Unit = {
-    if (row <= k){
+  private def pascal(lastRow: Int, row: Int, column: Int): Unit = {
+    if (row <= lastRow) {
       if (column <= row) {
-        print(s"${iterPascal(row, column)} ")
-        pascal(k, row, column + 1)
-      }
-      else{
+        print(s"${iterPascal(Block(row, column))} ")
+        pascal(lastRow, row, column + 1)
+      } else {
         println()
-        pascal(k, row+1, 1)
+        pascal(lastRow, row + 1, 1)
       }
     }
-
   }
-
-  def iterPascal(row: Int, column: Int): Int = {
-    if (column == 1 || column == row || row == 1) 1
-    else iterPascal(row - 1, column - 1) + iterPascal(row - 1, column)
+  private def iterPascal(block: Block): Int = {
+    if (block.column == 1 || block.column == block.row || block.row == 1) 1
+    else if (memoizeMap.contains(block)) memoizeMap(block)
+    else {
+      val upperLeftKey = Block(block.row - 1, block.column - 1)
+      val upperRightKey = Block(block.row - 1, block.column)
+      val upperLeft = iterPascal(upperLeftKey)
+      val upperRight = iterPascal(upperRightKey)
+      memoizeMap(upperLeftKey) = upperLeft
+      memoizeMap(upperRightKey) = upperRight
+      upperLeft + upperRight
+    }
   }
-
 }
